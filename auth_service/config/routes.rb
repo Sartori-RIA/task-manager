e49711883e2537c03695938def132e85cf414202
Lucas Sartori <faltou.criatividade0@gmail.com>
2024-08-05 14:02:45 -0300

    fix: docker compose volumes

--- auth_service/config/routes.rb
+++ auth_service/config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
+  devise_for :users
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
